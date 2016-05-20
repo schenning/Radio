4bb2bdf4bf5ff19fb16b882d28886b07d837eebd
schenning <henning.schei@gmail.com>
2016-05-20 13:16:52 +0200

    asd

---
+++ as3.m
@@ -0,0 +1,14 @@
+
+for i=1:1000
+	randnum()
+end
+
+
+
+
+
+%rayChanObj = rayleighchan(T_S, omega_max, 0, 0) ;
+%rayChanObj.StoreHistory = 1;
+%x = ones(nb_samples,1);
+%y = filter(rayChanObj,x);
+%g = rayChanObj.PathGains;
---
+++ randnum.m
@@ -0,0 +1,4 @@
+function res = randnum()
+	% Generates a number from a uniform distribution in the interval -pi:pi
+	res = rand(2*pi) - pi; 
+
